b1db6bedeefbdb6c16fb44bbadab468ec3097078
JohnnyWong-hub <61719644+JohnnyWong-hub@users.noreply.github.com>
2023-06-02 18:16:19 +0800

    Update app_main.c

--- src/app_main.c
+++ src/app_main.c
@@ -1,12 +1,25 @@
 #include "sys_common.h"
 #include "sys_logger.h"
 #include  "queue.h"
+
+
+//MSG_Q mq;
+
+//thread1
+//while(1)循环接收 50ms周期收
+
+//thread2
+//while(1)循环发送 50ms周期发
+
 INT32 app_component_init(VOID)
 {
     LOG_MESSAGE(LOG_INFO, "component init start");
 
     LOG_MESSAGE(LOG_ERROR, "component init finish");
-
+    //sys_queue_create(&mq)
+    //创建两个线程
+    //pthread_create(thread1) 接收线程
+    //phtered_create(thread2) 发送线程
     return 0;
 }
 
